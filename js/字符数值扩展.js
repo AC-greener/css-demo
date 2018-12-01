9b54ef8c99fdb8e97c82d8d6c658497f2e2712aa
tongtong <1053673440@qq.com>
2018-12-01 10:22:26 +0800

    'exercise'

---
+++ js/字符数值扩展.js
@@ -0,0 +1,26 @@
+for(let i of 'abcd') {
+  console.log(i)
+}
+
+let s = 'hello world'
+s.includes('h')
+s.startsWith('he')
+s.endsWith('ld')
+
+s.repeat(3)
+
+//二进制  0b开头
+0b11
+
+//八进制  0o开头
+0o11
+
+Number.isNaN(11)
+Number.isNaN(NaN)
+
+parseInt(1.2)
+
+Number.parseInt(1.2)
+
+//判断是否为整数
+Number.isInteger(1.2)
