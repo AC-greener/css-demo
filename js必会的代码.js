1af20a4d7f8d18b2dacf922323ee650f14bfb0e1
tongtong <1053673440@qq.com>
2019-09-18 09:50:11 +0800

    update

--- js必会的代码.js
+++ js必会的代码.js
@@ -1,22 +1,21 @@
 {
   //继承
-  function Parent(name) {
-    this.name = name
-  }
+    function Parent(name) {
+      this.name = name
+    }
 
-  Parent.prototype.getName = function () {
-    return this.name
-  }
+    Parent.prototype.getName = function () {
+      return this.name
+    }
 
-  function Child(name, age) {
-    Parent.call(this, name)
-    this.age = age
-  }
+    function Child(name, age) {
+      Parent.call(this, name)
+      this.age = age
+    }
 
-  Child.prototype = new Parent('qq')
+    Child.prototype = new Parent('qq')
 
   var instance = new Child('zhangsan', 18)
-  console.log(instance)
 
   function Parent(name) {
     this.name = name
@@ -35,65 +34,155 @@
   Child.prototype = Object.create(Parent.prototype) //创建了一个空对象，并用参数作为这个对象的__proto__
 
   var instance = new Child('zhangsan', 18)
-  console.log(instance)
 
 }
 {
   //发布订阅模式
-  let Observer = function() {
-    _message = {}
+  var Observer = function() {
+    let subs = {}
     return {
+      getSubs() {
+        return subs
+      },
       on(type, callback) {
-        if(_message[type]) {
-          _message[tyep] = [callback]
+        if(!subs[type]) {
+          subs[type] = [callback]
         } else {
-          _message[tyep].push(callback)
+          subs[type].push(callback)
         }
       },
       emit(type, args) {
-        if(!_message[type]) {
-          return 
-        }
-        let events = {
-          type,
-          args: args || {}
-        }
-        let length = _message[type].length
-        for(let i = 0; i < length; i++) {
-          _message[type][i].call(this, events)
+        if(subs[type]) {
+          subs[type].forEach(fn => {
+            fn.call(this, args)
+          })
+        } else {
+          throw new Error(`event ${type} not exist!`)
         }
       },
       remove(type) {
-        if(_message[type]) {
-          delete _message[type]
+        if(subs[type]) {
+          delete subs[type]
         }
       },
       once(type, callback) {
-        on(type, (args) => {
+        this.on(type, (args) => {
           callback(args)
-          remove(type)
+          this.remove(type)
         })
       }
     }
-  }
+  }()
 }
 
-//实现数组的forEach方法
-Array.prototype.forEach = function(fn) {
-  console.log(this)
-  let len = Array.length
-  for(let i = 0; i < len; i++) {
-    fn(this[i], i, this)
-  }
-}
-
-//原生AJAX
-let xhr = new XMLHttpRequest()
-xhr.open(method, url)
-xhr.onreadystatechange = function() {
-  if(xhr.readyState === 4) {
-    if(xhr.status >= 200 && xhr.status < 300) {
-      console.log(xhr.responseText)
+{
+  var a = [1, 2, 3]
+  //实现数组的forEach方法
+  Array.prototype.forEach = function(fn) {
+    // 这里的this为数组实例
+    let len = this.length
+    for(let i = 0; i < len; i++) {
+      fn(this[i], i, this)
     }
   }
+  // a.forEach(item => {
+  //   console.log(item)
+  // })
+
+  //实现数组的map方法
+  var a = [1, 2, 3]
+  Array.prototype.map = function(fn) {
+    let length = this.length
+    let result = []
+    for(let i = 0; i < length; i++) {
+      result.push(fn(this[i], i, this))
+    }
+    return result
+  }
+  a.map(item => {
+    return item * 2
+  })
+
+  //实现数组的filter方法
+  var a = [1, 2, 3]
+  Array.prototype.filter = function(fn) {
+    let length = this.length
+    let result = []
+    for(let i = 0; i < length; i++) {
+      if(fn(this[i], i, this)) {
+        result.push(this[i])
+      }
+    }
+    return result
+  }
+  a.filter(item => {
+    return item > 2
+  })
+
+  //实现数组的reduce方法
+  var a = [1, 2, 3]
+  Array.prototype.reduce = function(fn, initValue) {
+    if(!initValue) {
+      //表示没有传入初始值
+      let length = this.length
+      let accumulator
+      for(let i = 1; i < length; i++) {
+        if(i === 1) {
+          accumulator = fn(this[0], this[i], i, this)
+        } else {
+          accumulator = fn(accumulator, this[i], i, this)
+        }
+      }
+      return accumulator
+    } else {
+      let length = this.length
+      let accumulator = initValue
+      for(let i = 0 ; i < length; i++) {
+        accumulator = fn(accumulator, this[i], i, this)
+      }
+      return accumulator
+    }
+  }
+  console.log(  a.reduce((a, b) => {
+    return a + b
+  }, 10))
+  var b = [[0, 1], [2, 3], [4, 5, 6]]
+  console.log(b.reduce((total, currentValue) => {
+    return total.concat(currentValue)
+  }, []))
+}
+
+{
+  //原生AJAX
+// let xhr = new XMLHttpRequest()
+// xhr.open(method, url)
+// xhr.onreadystatechange = function() {
+ // 4是数据已经全部拿到，3是拿到了一部分 2是调用了send方法， 1是调用了open方法， 0是未调用open方法 
+//   if(xhr.readyState === 4) {
+//     if(xhr.status >= 200 && xhr.status < 300) {
+//       console.log(xhr.responseText)
+//     }
+//   }
+// }
+}
+{
+  //数组去重
+  var a = [1, 2, 2, 3, 3, 1, 5]
+  b = a.filter((value, index, arr) => {
+    return arr.indexOf(value) === index
+  })
+  a.reduce((accumulator, pre, index, arr) => {
+    if(arr.indexOf(pre) === index) {
+      accumulator.push(pre)
+    }
+    return accumulator
+  }, [])
+  var hash = {}
+  a.forEach((item, index) => {
+    if(!hash[item]) {
+      hash[item] = true
+    }
+  })
+  b = Object.keys(hash)
+
 }
